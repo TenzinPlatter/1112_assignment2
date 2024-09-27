db797a1259335a3631860f03e0b104cb4bbc1abe
TenzinPlatter <143778894+TenzinPlatter@users.noreply.github.com>
2024-09-27 12:10:07 +1000

    Added login funcitonality serverside

--- server.py
+++ server.py
@@ -1,14 +1,48 @@
 import sys
 import socket
 from threading import Thread
+from logins import Logins, Login
 
 class Client:
-    def __init__(self, name: str, sock: socket.socket) -> None:
-        self.name = name
+    def __init__(self, sock: socket.socket) -> None:
         self.socket = sock
+        self.account = None
+
+    def get_name(self) -> str | None:
+        """
+        Returns None if user is not logged in, else returns username
+        """
+        if not self.account:
+            return None
+
+        return self.account.get_name();
+
+
+    def try_login(self, user_input: str) -> None:
+        """
+        Scans data sent by connection until LOGIN message is sent with valid login
+        details
+        Pass in user_input as whole input without any cleaning
+        """
+        data = user_input.split(":")
+
+        if len(data) != 3:
+            self.socket.send("LOGIN:ACKSTATUS:3".encode())
+            return
+
+        account = Logins.try_login(data[0], data[1])
+
+        if isinstance(account, int):
+            self.socket.send(f"LOGIN:ACKSTATUS:{account}".encode())
+            return
+
+        self.account = account
+        # indicates successful login
+        self.socket.send("LOGIN:ACKSTATUS:0".encode())
 
 class Server:
     clients = []
+    rooms = []
 
     def __init__(self, host: str, port: int) -> None:
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -18,50 +52,40 @@ class Server:
         self.socket.listen()
         print("Server waiting for connection...")
 
-    """
-    Listens for connections and spawns a thread for each one
-    """
     def listen(self) -> None:
+        """
+        Listens for connections and spawns a thread for each one
+        """
         while True:
             conn, addr = self.socket.accept()
             print("Connection from: ", addr)
 
-            name = conn.recv(8192).decode()
-            client = Client(name, conn)
-
-            self.broadcast(name, name + " has joined the chat!")
+            client = Client(conn)
 
             Server.clients.append(client)
+
             # starts client in new thread
             Thread(target = self.handle_new_client, args = (client,)).start()
 
-    """
-    Broadcasts a message to all other members in server
-    """
     def broadcast(self, sender: str, msg: str):
+        """
+        Broadcasts a message to all other members in server
+        """
         for client in Server.clients:
-            if client.name == sender:
+            if client.name() == sender:
                 continue
 
             client.socket.send(msg.encode())
 
-    """
-    Function to handle client on a thread
-    """
     def handle_new_client(self, client: Client):
+        """
+        Function to handle client on a thread
+        """
         while True:
-            msg = client.socket.recv(1024).decode()
+            msg = client.socket.recv(8192).decode()
 
-            # empty message means closed connection
-            if not msg or msg == client.name + ": LEAVE":
-                self.broadcast(
-                        (client.name), f"{client.name} has left the chat!"
-                        )
-                Server.clients.remove(client)
-                client.socket.close()
-                break
-
-            self.broadcast(client.name, msg)
+            if msg.startswith("LOGIN:"):
+                client.try_login(msg)
 
 
 
