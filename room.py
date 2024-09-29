8cec360fdb174d85a9034c0774c09dd9afacf78d
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2024-09-29 20:02:00 +1000

    Finished roomlist

--- room.py
+++ room.py
@@ -4,5 +4,6 @@ class Room:
 class Rooms:
     pass
 
-    def get_room_names(self) -> list[str]:
-        return ["room 1", "awesome room (2)", "kill me please"]
+def get_room_names(self, is_player: bool) -> list[str]:
+    #TODO: implement check for wether room is full to see if player can join
+    return ["room 1", "awesome room (2)", "kill me please"]
